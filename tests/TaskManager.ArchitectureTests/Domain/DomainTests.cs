057ce7f405ab379190404bc623ec5a06e3e57a5f
Ezequiel Cardoso <62252654+ezequielcsilva@users.noreply.github.com>
2025-09-09 22:07:05 -0300

    test: add architecture tests

---
+++ tests/TaskManager.ArchitectureTests/Domain/DomainTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using NetArchTest.Rules;
+using System.Reflection;
+using TaskManager.ArchitectureTests.Infrastructure;
+using TaskManager.Domain.Abstractions;
+
+namespace TaskManager.ArchitectureTests.Domain;
+
+public class DomainTests : BaseTest
+{
+    [Fact]
+    public void Entities_ShouldHave_PrivateParameterlessConstructor()
+    {
+        IEnumerable<Type> entityTypes = Types.InAssembly(DomainAssembly)
+            .That()
+            .Inherit(typeof(Entity))
+            .GetTypes();
+
+        var failingTypes = (
+            from entityType in entityTypes
+            let constructors = entityType.GetConstructors(BindingFlags.NonPublic | BindingFlags.Instance)
+            where !constructors.Any(c => c.IsPrivate && c.GetParameters().Length == 0)
+            select entityType).ToList();
+
+        failingTypes.Should().BeEmpty();
+    }
+}
---
+++ tests/TaskManager.ArchitectureTests/Infrastructure/BaseTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Reflection;
+using TaskManager.Application.Abstractions.Messaging;
+using TaskManager.Domain.Abstractions;
+using TaskManager.Infrastructure;
+
+namespace TaskManager.ArchitectureTests.Infrastructure;
+
+public abstract class BaseTest
+{
+    protected static readonly Assembly ApplicationAssembly = typeof(ICommand).Assembly;
+
+    protected static readonly Assembly DomainAssembly = typeof(Entity).Assembly;
+
+    protected static readonly Assembly InfrastructureAssembly = typeof(ApplicationDbContext).Assembly;
+
+    protected static readonly Assembly PresentationApiAssembly = typeof(Program).Assembly;
+}
